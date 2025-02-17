80a886b2585b5d8e3296e25acd9ca486faaac223
SimoesE2024 <edgarsimoes52@gmail.com>
2025-02-17 03:17:13 +0200

    pagina com Creditos a funcionar

--- edgars/urls.py
+++ edgars/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+#from pages.views import CreditoDiarioView, CreditoPlusView, CreditoRelampagoView, HomePageView, signup
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('usuarios/', include('django.contrib.auth.urls')),  # Adiciona todas as URLs de autenticação do Django
+    
 ]
