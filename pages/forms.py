fe46267ae8ea93742d694d7b41a3a5d9ba262291
SimoesE2024 <edgarsimoes52@gmail.com>
2025-07-19 20:39:54 +0200

    Adicionado o template solicitar_credito.html e ajuste na view

--- pages/forms.py
+++ pages/forms.py
@@ -5,10 +5,17 @@ class SolicitationForm(forms.Form):
     email = forms.EmailField(label='Email')
     phone = forms.CharField(max_length=15, label='Telefone')
     amount = forms.IntegerField(label='Valor Solicitado')
-    installments = forms.IntegerField(label='Número de Parcelas', min_value=1, max_value=36)  # Máximo de 36 parcelas
+    installments = forms.IntegerField(
+        label='Número de Parcelas', min_value=1, max_value=36
+    )
     collateral = forms.CharField(max_length=200, label='Bem a Penhorar')
-    
-def clean_amount(self):
+
+    # NOVOS CAMPOS DE UPLOAD
+    document_bi = forms.FileField(label='Cópia do BI', required=True)
+    document_income = forms.FileField(label='Declaração de Rendimento', required=True)
+    photo_collateral = forms.ImageField(label='Foto do Bem a Penhorar', required=True)
+
+    def clean_amount(self):
         amount = self.cleaned_data.get("amount")
         if amount > 5000:
             raise forms.ValidationError("O valor máximo permitido é 5.000!")
